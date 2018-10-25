cbee792534a3688842fb972b82566a9284e4e360
messmerd <messmerd97@gmail.com>
2018-10-25 16:50:58 +0200

    Added pushbutton code to start test. Added a second analog thermocouple. Added old analog thermocouple code, but it is commented out.

--- main/main.ino
+++ main/main.ino
@@ -14,10 +14,13 @@ extern Config conf; // A struct containing the config file info
 #define MAXCLK    5
 
 #define LED_PIN 9                 // Pin 12 isn't working... Maybe it has to do with the SD card reader? 
-#define PUSHBUTTON_PIN 11  
+#define PUSHBUTTON_PIN 11         // 
 #define chipSelect 10             // For the SD card reader
 #define THERMOCOUPLE_PIN_0 A0     // This identifies the port the data is collected from.
+#define THERMOCOUPLE_PIN_1 A1     // This identifies the port the data is collected from.
+
 #define NUMSAMPLES 5              // This is a random sample amount
+//uint16_t samples[NUMSAMPLES];
 
 unsigned int _timer = 0;
 unsigned int _timer_max = 1000; 
@@ -26,6 +29,8 @@ boolean led_value = 0;
 
 void initTimer0(double seconds);
 double collectAnalogThermocoupleData(int pin, int samples = 5);
+//double collectAnalogThermocoupleDataOld(int pin, int samples = 5);
+
 //double collectDigitalThermocoupleData(Adafruit_MAX31855 thermo);
 void printToFile(char *filename, String text, boolean append = true);
 
@@ -56,6 +61,9 @@ void setup() {
   initTimer0(conf.sample_rate);
 
   delay(500); // Just in case things need to settle
+  
+  Serial.println("Press pushbutton to start test.");
+  while (!digitalRead(PUSHBUTTON_PIN)) {; };  // Start test when pushbutton is pressed. (STILL NEED TO TEST) 
 }
 
 void loop() {
@@ -67,7 +75,11 @@ void loop() {
     led_value=!led_value;
     _timer = 0;
 
-    String dataString = "a: " + (String)collectAnalogThermocoupleData(THERMOCOUPLE_PIN_0, NUMSAMPLES) + " C. d0: " + (String)digital_thermo_0.readCelsius() + " C. d1: " + (String)digital_thermo_1.readCelsius() + " C."; 
+    String dataString = "a0: " + (String)collectAnalogThermocoupleData(THERMOCOUPLE_PIN_0, NUMSAMPLES) + " C.\n";
+    dataString += "a1: " + (String)collectAnalogThermocoupleData(THERMOCOUPLE_PIN_1, NUMSAMPLES) + " C.\n";
+    dataString += "d0: " + (String)digital_thermo_0.readCelsius() + " C.\n"; 
+    dataString += "d1: " + (String)digital_thermo_1.readCelsius() + " C.\n\n"; 
+    //Serial.println(dataString);
     printToFile(DATALOG_FILE, dataString); // print to file
   }
   
@@ -76,7 +88,7 @@ void loop() {
 
 double collectAnalogThermocoupleData(int pin, int num_samples = 5)
 {
-  uint8_t i;
+  uint8_t i = 0;
   double average = 0.0; // Initializes average as a variable
 
   for(i=0; i<num_samples; i++) { // This loop gathers a certain number of samples and sums them up
@@ -90,6 +102,36 @@ double collectAnalogThermocoupleData(int pin, int num_samples = 5)
   // final calculations based on the thermocouple chip math. Units are Celcius
   return 0.98*(average/num_samples) - 250.0; 
 }
+
+/*
+double collectAnalogThermocoupleDataOld(int pin, int num_samples = 5)
+{
+  uint8_t i;
+  float average; // Initializes average as a variable
+
+  for(i=0; i< NUMSAMPLES;i++) { // This loop gathers 5 samples and stores them in an array
+    samples[i] = analogRead(pin); // this part reads info in from the thermocouple
+    //delay(10);
+  }
+
+  average = 0;  // This sets average to 0
+  for (i=0; i< NUMSAMPLES; i++) { // takes 5 samples
+    average += samples[i]; // sets average to the total of the 5 amounts in the average array
+  }
+  average /= NUMSAMPLES; // divides average by 5 so it accurately represents the average
+
+  average = average *4.9; // confusing, but the information is stored on a 1023 scale, where each value of 1-1023 represents 4.9mV.  So multiplying by 4.9 converts it to mV
+
+  float temp; // initializes the variable temp
+  temp = (average - 1250.0)/5.0; // final calculations based on the thermocouple chip math
+
+  Serial.print("Temperature "); // These output the temperature
+  Serial.print(temp);
+  Serial.println(" *C");
+}
+
+*/
+
 
 void printToFile(char *filename, String text, boolean append = true)
 {
